e68627591866eb566b05e5248f975799aef17f12
Sergey Mityanin <mityanin@sm.org.ru>
2018-03-28 14:43:07 +0300

    Improve

--- app.js
+++ app.js
@@ -1,103 +1,54 @@
-var amqp = require('amqplib/callback_api')
 var redis = require('redis')
 var redisClient = null
 var redisSubscriber = null
-var amqpConn = null
-var pubChannel = null
 
 function start () {
-  amqpConn = null
-  pubChannel = null
-  amqp.connect(process.env.AMQP_URL + '?heartbeat=60', function (err, conn) {
-    if (err) {
-      console.error('[AMQP]', err.message)
-      return setTimeout(start, 500)
-    }
-    conn.on('error', function (err) {
-      if (err.message !== 'Connection closing') {
-        console.error('[AMQP] conn error', err.message)
+  redisSubscriber = redis.createClient('redis://redis')
+  redisClient = redis.createClient('redis://redis')
+  redisClient.on('error', function (err) {
+    console.log('[REDIS client] ' + err)
+  })
+  redisSubscriber.on('error', function (err) {
+    console.log('[REDIS subscriber] ' + err)
+  })
+  redisSubscriber.psubscribe('__keyevent@0__:expired')
+  redisSubscriber.on('pmessage', function (pattern, channel, messageId) {
+    console.info('[REDIS subscriber] Message ' + messageId + ' expired')
+    redisClient.hgetall(messageId + '_clone', function (err, reply) {
+      if (err) {
+        console.log('[REDIS client] ' + err)
       }
-    })
-    conn.on('close', function () {
-      console.error('[AMQP] reconnecting')
-      return setTimeout(start, 500)
-    })
-    console.log('[AMQP] connected')
-    amqpConn = conn
-    redisSubscriber = redis.createClient('redis://redis')
-    redisClient = redis.createClient('redis://redis')
-    redisClient.on('error', function (err) {
-      console.log('[REDIS client] ' + err)
-    })
-    redisSubscriber.on('error', function (err) {
-      console.log('[REDIS subscriber] ' + err)
-    })
-    redisSubscriber.psubscribe('__keyevent@0__:expired')
-    redisSubscriber.on('pmessage', function (pattern, channel, messageId) {
-      console.info('[REDIS subscriber] Message ' + messageId + ' expired')
-      redisClient.hgetall(messageId + '_clone', function (err, reply) {
-        if (err) {
-          console.log('[REDIS client] ' + err)
-        }
-        if (reply) {
-          publishToQueue(reply.queue, reply.message, messageId, reply.correlationId, function () {
-            console.log('[REDIS] Message requeued ' + messageId)
-            redisClient.multi()
-              .hdel(messageId + '_clone', 'message')
-              .hdel(messageId + '_clone', 'queue')
-              .hdel(messageId + '_clone', 'correlationId')
-              .exec(function (err, replies) {
-                if (err) {
-                  console.log('[REDIS client] ' + err)
-                }
-              })
-          })
-        } else {
-          console.log('[REDIS client] No such message')
-        }
-      })
+      if (reply) {
+        publishToQueue(reply.queue, reply.message, messageId, reply.correlationId, reply.publishTime, function () {
+          console.log('[REDIS] Message requeued ' + messageId)
+        })
+      } else {
+        console.log('[REDIS client] No such message')
+      }
     })
   })
 }
 
-function publishToQueue (queue, content, messageId, correlationId, callback) {
+function publishToQueue (queue, content, messageId, correlationId, publishTime, callback) {
   try {
-    if (!pubChannel) {
-      amqpConn.createConfirmChannel(function (err, ch) {
-        if (err) {
-          console.error('[AMQP]', err.message)
-          return setTimeout(start, 500)
-        }
-        ch.on('error', function (err) {
-          console.error('[AMQP] channel error', err.message)
-        })
-        ch.on('close', function () {
-          console.log('[AMQP] channel closed')
-          pubChannel = null
-          return setTimeout(start, 500)
-        })
-        pubChannel = ch
-        pubChannel.sendToQueue(queue, Buffer.from(content), { persistent: true, messageId: messageId, correlationId: correlationId },
-                          function (err, ok) {
-                            if (err !== null) {
-                              console.error('[AMQP] publish', err)
-                            } else {
-                              callback()
-                            }
-                          })
-      })
-    } else {
-      pubChannel.sendToQueue(queue, Buffer.from(content), { persistent: true, messageId: messageId, correlationId: correlationId },
-                      function (err, ok) {
-                        if (err !== null) {
-                          console.error('[AMQP] publish', err)
-                        } else {
-                          callback()
-                        }
-                      })
-    }
+    redisClient.multi()
+    .hdel(messageId + '_clone', 'message')
+    .hdel(messageId + '_clone', 'queue')
+    .hdel(messageId + '_clone', 'correlationId')
+    .hset(messageId, 'message', content)
+    .hset(messageId, 'correlationId', correlationId)
+    .hset(messageId, 'queue', queue)
+    .hset(messageId, 'publishTime', publishTime)
+    .zadd(queue, publishTime, messageId)
+    .exec(function (err, replies) {
+      if (err) {
+        console.error('[REDIS publish] error ', err)
+      } else {
+        callback()
+      }
+    })
   } catch (e) {
-    console.error('[AMQP] publish', e.message)
+    console.error('[REDIS] publish', e.message)
     callback()
   }
 }
